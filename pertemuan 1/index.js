b9f2fb80eb90550d81f217d4fbe051d66db12fe0
delonikr <delonikr@gmail.com>
2025-03-18 14:51:39 +0700

    upload

---
+++ pertemuan 1/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Tampilkan Data')
+})
+
+app.post('/', function (req, res) {
+  res.send('Tambah Data')
+})
+
+app.put('/', function (req, res) {
+  res.send('Edit Data')
+})
+
+app.delete('/', function (req, res) {
+  res.send('Hapus Data')
+})
+
+app.listen(3000)
